e05a4904e3400179ce3f7e2d2489ae2a988d6d9c
Jasper Hjorth <jasper.hjorth@gmail.com>
2023-05-24 09:59:36 +0300

    Add statistic features

--- main_page.py
+++ main_page.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import requests
+import os
+import json
 
 st.markdown('# Pääsivu')
 st.sidebar.markdown("# Pääsivu")
+
+BACKEND_URL = os.getenv("BACKEND_URL")
+
+data = requests.get(f"{BACKEND_URL}/dataset_names/")
+datasets = json.loads(data.text)
+data = requests.get(f"{BACKEND_URL}/registered_devices/")
+devices = json.loads(data.text)
+
+
+st.write( devices.count("id"), " Devices registered")
+st.write(len(datasets["dataset_names"]), " Datasets saved")
