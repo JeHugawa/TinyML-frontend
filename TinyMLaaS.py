5b6fdee6fe6aef717202a824f58c44c594c25acb
shlevanto <simo.levanto@helsinki.fi>
2023-05-26 16:10:10 +0300

    Frontend cleanup

--- TinyMLaaS.py
+++ TinyMLaaS.py
@@ -2,20 +2,39 @@ import streamlit as st
 import requests
 import os
 import json
+import plotly.express as px
+import pandas as pd
 
 from dotenv import load_dotenv
 load_dotenv()
 
-st.markdown('# Pääsivu')
-st.sidebar.markdown("# Pääsivu")
-
 BACKEND_URL = os.getenv("BACKEND_URL")
+
+
+# Page setup
+st.set_page_config(
+    page_title="TinyMLaaS",
+    layout="wide"
+)
+st.title("Welcome to TinyMLaaS")
+
+st.header("Device Location Map")
+device_locations = pd.DataFrame({
+    "device_id": ["Arduino 1", "Arduino 2", "Raspberry pi 2", "undefined device"],
+    "latitude": [60.203978, 60.208609, 60.207861, 60.201926],
+    "longitude": [24.961129, 24.966743, 24.965956, 24.968977],
+    "last_update": ["2022-03-01 12:30:00", "2022-03-01 14:45:00", "2022-03-01 13:00:00", "2022-03-01 11:00:00"]
+})
+
+st.map(device_locations[["latitude", "longitude"]], zoom=13)
+
+st.header("Statistical Data")
 
 data = requests.get(f"{BACKEND_URL}/dataset_names/")
 datasets = json.loads(data.text)
+
 data = requests.get(f"{BACKEND_URL}/registered_devices/")
 devices = json.loads(data.text)
-
 
 st.write(devices.count("id"), " Devices registered")
 st.write(len(datasets["dataset_names"]), " Datasets saved")
---
+++ pages/02_Data.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from services import data_service
+
+st.set_page_config(
+    page_title="Data", 
+    layout="wide"
+    )
+
+# List all registered devices
+st.header("Existing datasets")
+name_data = data_service.get_dataset_names()
+st.table(name_data)
+
+st.header("Data")
+
+if "selected_dataset" not in st.session_state:
+    st.session_state["selected_dataset"] = None
+    
+data = data_service.get_dataset_names_size()
+st.session_state["selected_dataset"] = st.selectbox("Select a dataset: ", data, format_func=data_service.format_datasets)
+
+st.success(f"You selected: {st.session_state['selected_dataset']['name']} dataset, size {st.session_state['selected_dataset']['size']}")
