cfaa1f06010e7206d66999a3e26e9be4659b1918
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-02 12:25:53 +0300

    Fixed code quality for pages 04-08

--- pages/05_Compiling.py
+++ pages/05_Compiling.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Compiling", 
+    page_title="Compiling",
     layout="wide"
-    )
+)
 
 st.header("Model compilation makes them tiny")
--- pages/07_Observing.py
+++ pages/07_Observing.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Observing", 
+    page_title="Observing",
     layout="wide"
-    )
+)
 
 st.header("Gather observations from MCU")
