706da230caca6ddccc246cb395bcc178e98125da
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-02 13:31:55 +0300

    Data service code quality fixed for pylint

--- services/data_service.py
+++ services/data_service.py
@@ -1,17 +1,17 @@
-import requests
 import json
+import requests
 
 from config import BACKEND_URL
 
 
 def get_dataset_names():
-    response = requests.get(f"{BACKEND_URL}/dataset_names/")
+    response = requests.get(f"{BACKEND_URL}/dataset_names/", timeout=5)
     data = json.loads(response.text)
     return data
 
 
 def get_dataset_names_size():
-    response = requests.get(f"{BACKEND_URL}/dataset_names_size/")
+    response = requests.get(f"{BACKEND_URL}/dataset_names_size/", timeout=5)
     data = json.loads(response.text)
     return data
 
