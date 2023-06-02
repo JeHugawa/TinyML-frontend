a75564d14edb133b2c8f93fb333777d2aca7aa47
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-02 13:42:41 +0300

    Bridge service change import order, timeout to backend calls

--- services/bridge_service.py
+++ services/bridge_service.py
@@ -1,6 +1,6 @@
-import requests
 import os
 import json
+import requests
 import pandas as pd
 
 
@@ -13,7 +13,7 @@ def add_bridge(address: str, name: str = None):
         "name": name
     }
     data = {key: val if len(val) > 0 else None for key, val in data.items()}
-    response = requests.post(f"{BACKEND_URL}/bridges/", json=data)
+    response = requests.post(f"{BACKEND_URL}/bridges/", json=data, timeout=5)
     if response.status_code == 201:
         return None
     return json.loads(response.text)
@@ -27,7 +27,7 @@ def remove_bridge(*args):
     """
 
     bridge_id = ''.join(args)
-    response = requests.delete(f"{BACKEND_URL}/bridges/{bridge_id}")
+    response = requests.delete(f"{BACKEND_URL}/bridges/{bridge_id}", timeout=5)
 
     if response.status_code == 400:
         raise ValueError()
@@ -36,21 +36,20 @@ def remove_bridge(*args):
 def get_registered_bridges():
     """Return a list of all registered devices on backend"""
 
-    response = requests.get(f"{BACKEND_URL}/bridges/")
+    response = requests.get(f"{BACKEND_URL}/bridges/", timeout=5)
 
     if response.text == []:
         raise ValueError()
-    else:
-        df = pd.read_json(response.text)
+    dataframe = pd.read_json(response.text)
 
-    return df
+    return dataframe
 
 
 def try_conntection(address: str):
     if "http" not in address:
         address = "http://" + address
     try:
-        requests.get(address)
+        requests.get(address, timeout=5)
         return True
     except requests.exceptions.ConnectionError:
         return False
