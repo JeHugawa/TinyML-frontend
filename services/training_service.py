129ddbf15227000a5f0f412a71b2261e1c4818c9
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-06 13:11:37 +0300

    Timeout for reading to None, decode image from json

--- services/training_service.py
+++ services/training_service.py
@@ -1,5 +1,6 @@
 import os
 import json
+import base64
 import requests
 import pandas as pd
 
@@ -7,17 +8,20 @@ import pandas as pd
 BACKEND_URL = os.getenv("BACKEND_URL")
 
 
-def train_model(dataset_id: int,model_name: str, epochs: int,
-                img_width: int, img_height: int, batch_size: int,lossfunc: str):
+def train_model(dataset_id: int, model_name: str, epochs: int,
+                img_width: int, img_height: int, batch_size: int, lossfunc: str):
     data = {
-        "model_name": model_name, 
+        "model_name": model_name,
         "epochs": epochs,
         "img_width": img_width,
         "img_height": img_height,
         "batch_size": batch_size
     }
     response = requests.post(f"{BACKEND_URL}/model/dataset/{dataset_id}?lossfunc={lossfunc}",
-                             json=data, timeout=5)
+                             json=data, timeout=(5, None))
+    res = response.text.strip('][').split(', ')
+    response_dict = json.loads(res[0])
+    img = base64.b64decode(response_dict["image"])
     if response.status_code == 201:
         return None
     return json.loads(response.text)
