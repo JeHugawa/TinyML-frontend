f380164d469b7ade3245b6da214b8c7a4fc0cfdc
Tuomas Marenk <tuomas.marenk@gmail.com>
2023-06-07 15:27:12 +0300

    Get models from backend

--- pages/03_Model.py
+++ pages/03_Model.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
+from datetime import datetime
 from pages.sidebar import sidebar
+from services import model_service
 
 state = st.session_state
 
@@ -11,4 +12,37 @@ st.set_page_config(
 
 sidebar.load_side_bar()
 
-st.header("Available machine learning models")
+
+def load_page_info():
+    st.header("Available machine learning models")
+
+
+def list_trained_models():
+    models = model_service.get_models()
+
+    col = st.columns(11)
+    col[0].write("ID")
+    col[1].write("Description")
+    col[2].write("Epochs")
+    col[3].write("Batch size")
+    col[4].write("Dataset")
+    col[5].write("Created")
+    for model in models:
+        dataset_id, parameters, description, id, created, _ = model.values()
+        created = datetime.fromisoformat(created)
+        created = created.strftime("%d/%m/%Y %H:%M")
+        col[0].write(id)
+        col[1].write(description)
+        col[2].write(parameters["epochs"])
+        col[3].write(parameters["batch_size"])
+        col[4].write(dataset_id)
+        col[5].write(created)
+
+
+def main():
+    load_page_info()
+
+    list_trained_models()
+
+
+main()
