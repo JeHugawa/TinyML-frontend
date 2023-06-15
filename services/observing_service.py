ede9b473030bfa39e907c3938c2ecb6778bc8865
Jasper Hjorth <jasper.hjorth@gmail.com>
2023-06-15 15:18:19 +0300

    Hook observing service to use backend

--- services/observing_service.py
+++ services/observing_service.py
@@ -10,8 +10,8 @@ def observe_device(device_id):
     if os.environ.get("ROBOT_TESTS") == "true":
         return("%.2f" % random.uniform(0, 1))
 
-
-    return(0)
+    response = requests.GET(f"{BACKEND_URL}/bridges/ /devices/{device_id}", timeout = 2).json()
+    return(response['observation_value'])
 
 
 def observe_device_with_dataset_feed(device_id):
